d59bb38e79caac5e06815f7c83d2bb18ec4d1635
Robert Cauvy <robert.cauvy@wework.com>
2018-02-11 15:47:32 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,17 @@
 # Write your #display_rainbow method here
+
+def 
+  
+  #display_rainbow (["red","orange","yellow","green","blue", "indigo","violet"])
+
+  
+  puts "#{x[0]}"
+  puts "#{x[1]}"
+  puts "#{x[2]}"
+  puts "#{x[3]}"
+  puts "#{x[4]}"
+  puts "#{x[5]}"
+  puts "#{x[6]}"
+  
+end
+end
